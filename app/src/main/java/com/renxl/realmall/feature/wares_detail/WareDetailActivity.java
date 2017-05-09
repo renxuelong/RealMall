f2a9f129a63675deb11bb27c263fa8ae5622a349
renxl <renxuelongvip@163.com>
2017-05-09 13:47:59 +0800

    商品详情页开发

---
+++ app/src/main/java/com/renxl/realmall/feature/wares_detail/WareDetailActivity.java
@@ -0,0 +1,12 @@
+package com.renxl.realmall.feature.wares_detail;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WareDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
